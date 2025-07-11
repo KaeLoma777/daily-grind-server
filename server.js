32bfb5bc91677f2d679db01330674756acd55b9d
Nneka <777uloma777@gmail.com>
2025-07-10 21:46:59 -0400

    added /api/fun-fact using axios

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import url from 'url'
+import axios from 'axios';
 
 const fileName = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(fileName)
@@ -19,6 +20,16 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'))
 })
 
+app.get('/api/fun-fact', async (req,res) => {
+    try{
+        const response = await axios.get('https://uselessfacts.jsph.pl//api/v2/facts/random');
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching fun fact:', error.message);
+        res.status(500).json({error: 'Failed to fetch fun fact'});
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
-})
+});
